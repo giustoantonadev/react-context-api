b54debcbf83541f57be5306368610613845f6771
giustoantonadev <giustoantonadev@gmail.com>
2026-03-24 16:28:00 +0100

    Add budget button in component navbar

--- src/contexts/BudgetContext.jsx
+++ src/contexts/BudgetContext.jsx
@@ -2,9 +2,8 @@ import { createContext, useState } from "react";
 
 export const BudgetContext = createContext()
 
-
 export const BudgetProvider = ({ children }) => {
- 
+
     const [budgetMode, setBudgetMode] = useState(false)
 
     return (
--- src/layouts/AppNavBar.jsx
+++ src/layouts/AppNavBar.jsx
@@ -1,11 +1,23 @@
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import navMenu from "../data/navMenu"
+import { useContext } from "react"
+import { BudgetContext } from "../contexts/BudgetContext"
 
 export default function AppNavBar() {
+
+    const { budgetMode, setBudgetMode } = useContext(BudgetContext)
+
     return (
 
         <nav className="py-3 gap-5 navbar bg-secondary justify-content-center" >
             {navMenu.map(item => <NavLink to={item.path} key={item.id} >{item.text}</NavLink>)}
+            <button
+                onClick={() => setBudgetMode(!budgetMode)}
+                className="btn btn-secondary bg-white text-danger">
+                <strong>
+                    {budgetMode ? "Disattiva Modalità Budget" : "Attiva Modalità Budget"}
+                </strong>
+            </button>
         </nav>
 
     )
